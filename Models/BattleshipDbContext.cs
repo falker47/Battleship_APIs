54bd2aec8e4a31de5fc0bef5827be68ee7a6827a
Flavio Gioia <flaviog2@hotmail.it>
2023-04-19 22:57:15 +0200

    fixed some problems

--- Models/BattleshipDbContext.cs
+++ Models/BattleshipDbContext.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Battleship_APIs.Models;
 
 public partial class BattleshipDbContext : DbContext
 {
-    private MySettings _settings;
-    var localConfig = new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory).AddJsonFile("appsettings.json", false).Build();
 
     public BattleshipDbContext()
     {
@@ -31,7 +26,7 @@ public partial class BattleshipDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer(localConfig.GetConnectionString("localConnectionString"));
+        => optionsBuilder.UseSqlServer("Server=localhost;Database=BattleshipDB;Trusted_Connection=True; TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
