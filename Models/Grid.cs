01e0c435adce4a6b4f4e1cb1eafa38bee46a40f0
Flavio Gioia <92399059+LoggiExp@users.noreply.github.com>
2023-04-19 20:13:54 +0200

    Database test -> develop (#1)
    * Aggiunto db
    
    * removed useless things
    
    ---------
    
    Co-authored-by: RedGuardiaN7 <artiom.str@hotmail.it>

---
+++ Models/Grid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Battleship_APIs.Models;
+
+public partial class Grid
+{
+    public byte Id { get; set; }
+
+    public byte? PlayerId { get; set; }
+
+    public virtual ICollection<Cell> Cells { get; set; } = new List<Cell>();
+
+    public virtual Player? Player { get; set; }
+
+    public virtual ICollection<Player> PlayerShotGrids { get; set; } = new List<Player>();
+
+    public virtual ICollection<Player> PlayerUserGrids { get; set; } = new List<Player>();
+}
---
+++ Models/Ship.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Battleship_APIs.Models;
+
+public partial class Ship
+{
+    public byte Id { get; set; }
+
+    public byte PlayerId { get; set; }
+
+    public byte Length { get; set; }
+
+    public byte Hp { get; set; }
+
+    public virtual Player Player { get; set; } = null!;
+}
